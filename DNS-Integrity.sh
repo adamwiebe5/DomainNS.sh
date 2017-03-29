94b8db1a5a16a457a77ce717eb8854d1096e764a
Adam Wiebe <adamwiebe@Adams-iMac.local>
2017-03-29 09:14:13 -0500

    Renaming scripts.

---
+++ DNS-Integrity.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Usage: ./DomainNS.sh [DomainName-List] > /File-Out
+#
+# The input file needs to be a simple, two column list of DNS names, and the record type (A,TXT,SRV)
+#
+
+queryServer="129.130.254.2"
+fqdnList=`cat $1`
+
+while read i; do
+
+IFS=',' read y z <<<"$i"
+
+currentRecord="$y"
+recordType="$z"
+
+digResult=`dig @"$queryServer" $currentRecord $recordType +short`
+digResult="${digResult//$'\n'/;}"
+printf "$recordType"','"$currentRecord"','"$digResult\n"
+
+done <<< "$fqdnList"
+
