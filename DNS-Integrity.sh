9309e2cd4f940ff7433a4d23e2f0fa6f23c1eaeb
Adam Wiebe <adamwiebe@Adams-iMac.local>
2017-10-19 11:27:55 -0500

    Updating DNS-Integrity Script

--- DNS-Integrity.sh
+++ DNS-Integrity.sh
@@ -5,19 +5,19 @@
 # The input file needs to be a simple, two column list of DNS names, and the record type (A,TXT,SRV)
 #
 
-queryServer="129.130.254.2"
+queryServer="10.139.43.255"
 fqdnList=`cat $1`
 
 while read i; do
-
 IFS=',' read y z <<<"$i"
 
 currentRecord="$y"
 recordType="$z"
-
-digResult=`dig @"$queryServer" "$currentRecord" "$recordType" +short`
+digResult=`dig @"$queryServer" "$currentRecord" "$recordType" +short | sort`
 digResult="${digResult//$'\n'/;}"
-echo "$recordType"',,'"$currentRecord"','"$digResult"
+digResult="${digResult//';; Truncated, retrying in TCP mode.'/}"
+
+echo "$currentRecord,"$digResult",$recordType"
 
 done <<< "$fqdnList"
 
