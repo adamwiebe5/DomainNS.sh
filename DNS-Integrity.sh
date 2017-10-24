e00991b3fccc1292100b41038b079bb051fbd365
Adam Wiebe <adamwiebe@Adams-iMac.local>
2017-10-24 07:37:48 -0500

    Updating

--- DNS-Integrity.sh
+++ DNS-Integrity.sh
@@ -6,18 +6,19 @@
 #
 
 queryServer="10.139.43.255"
-fqdnList=`cat $1`
+fqdnList=`cat $1 | sed -e 's/[^ -~]//g'`
 
 while read i; do
 IFS=',' read y z <<<"$i"
 
 currentRecord="$y"
 recordType="$z"
-digResult=`dig @"$queryServer" "$currentRecord" "$recordType" +short | sort`
+
+digResult=$(dig @$queryServer $currentRecord "$recordType" +short | sort)
 digResult="${digResult//$'\n'/;}"
 digResult="${digResult//';; Truncated, retrying in TCP mode.'/}"
 
-echo "$currentRecord,"$digResult",$recordType"
+echo $currentRecord,$digResult,$recordType
 
 done <<< "$fqdnList"
 
